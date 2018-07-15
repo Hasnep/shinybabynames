27e37bfaa867b14b6511b24835edabdd8b90895c
Hannes <hasnep@gmail.com>
2018-07-15 12:55:19 +0800

    Create ui

--- app.R
+++ app.R
@@ -8,7 +8,35 @@ ui <- fluidPage(
   titlePanel("Baby Names"),
   sidebarLayout(
     sidebarPanel(
-      # place inputs here
+      textInput(inputId = "names",
+                label = "Names",
+                value = "Hannes",
+                placeholder = "Enter the names seperated by spaces or plusses to combine"
+      ),
+      radioButtons(inputId = "sex",
+                   label = "Sex",
+                   choiceNames = c("Both", "Female", "Male"),
+                   choiceValues = c(TRUE, "F", "M"),
+                   inline = TRUE
+      ),
+      sliderInput(inputId = "xrange",
+                  label = "X-axis range",
+                  min = 1880, 
+                  max = as.integer(format(Sys.Date(), "%Y")),
+                  value = c(1900, 2015),
+                  round = TRUE,
+                  sep = ""
+                  ),
+      radioButtons(inputId = "yaxis",
+                   label = "Y-axis values",
+                   choiceNames = c("Proportion", "Number"),
+                   choiceValues = c("prop", "n"),
+                   inline = TRUE
+      ),
+      numericInput(inputId = "minbirths",
+                   label = "Minimum number of births",
+                   value = 10,
+                   min = 5) 
     ),
     mainPanel(
       plotOutput("main_plot")
@@ -17,10 +45,7 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-  
-  output$main_plot <- renderPlot({
-# place plot code here
-  })
+ 
 }
 
 shinyApp(ui = ui, server = server)
