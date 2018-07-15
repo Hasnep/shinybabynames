dc02055fa2b73c101234d9ee241a20d3664690d9
Hannes <hasnep@gmail.com>
2018-07-15 11:51:57 +0800

    Clean up main app file ready for building

--- app.R
+++ app.R
@@ -1,41 +1,26 @@
+library(babynames)
 library(shiny)
+library(ggplot2)
+library(magrittr)
+library(dplyr)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+  titlePanel("Baby Names"),
+  sidebarLayout(
+    sidebarPanel(
+      # place inputs here
+    ),
+    mainPanel(
+      plotOutput("main_plot")
+    )
+  )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  
+  output$main_plot <- renderPlot({
+# place plot code here
+  })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
-
