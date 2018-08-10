02df45f291ca872a9c4e4cdeb43839d3dd7c1774
Hasnep <hasnep@gmail.com>
2018-08-10 14:12:06 +0800

    Clean up code

--- app.R
+++ app.R
@@ -14,34 +14,40 @@ ui <- fluidPage(
   titlePanel("Baby Names"),
   sidebarLayout(
     sidebarPanel(
-      textInput(inputId = "names",
-                label = "Names",
-                value = "Mary"
+      textInput(
+        inputId = "names",
+        label = "Names",
+        value = "Mary"
       ),
-      radioButtons(inputId = "sex",
-                   label = "Sex",
-                   choiceNames = c("Both", "Female", "Male"),
-                   choiceValues = c("B", "F", "M"),
-                   inline = TRUE
+      radioButtons(
+        inputId = "sex",
+        label = "Sex",
+        choiceNames = c("Both", "Female", "Male"),
+        choiceValues = c("B", "F", "M"),
+        inline = TRUE
       ),
-      sliderInput(inputId = "xrange",
-                  label = "X-axis range",
-                  min = 1880,
-                  max = 2015,
-                  value = c(1900, 2015),
-                  step = 5,
-                  sep = ""
+      sliderInput(
+        inputId = "xrange",
+        label = "X-axis range",
+        min = 1880,
+        max = 2015,
+        value = c(1900, 2015),
+        step = 5,
+        sep = ""
       ),
-      radioButtons(inputId = "yaxis",
-                   label = "Y-axis values",
-                   choiceNames = c("Proportion", "Number"),
-                   choiceValues = c("prop", "n"),
-                   inline = TRUE
+      radioButtons(
+        inputId = "yaxis",
+        label = "Y-axis values",
+        choiceNames = c("Proportion", "Number"),
+        choiceValues = c("prop", "n"),
+        inline = TRUE
       ),
-      numericInput(inputId = "minbirths",
-                   label = "Minimum number of births",
-                   value = 5,
-                   min = 5),
+      numericInput(
+        inputId = "minbirths",
+        label = "Minimum number of births",
+        value = 5,
+        min = 5
+      ),
       hr(),
       p("Baby name data for the USA from 1880 to 2015 provided by the SSA. This includes all names with at least 5 uses.")
     ),
@@ -53,30 +59,31 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-  
+
   # Filter the data
   filtered_data <- reactive({
     plot_names <- input$names %>%
       tolower(.) %>%
       strsplit(., "[ ,]+") %>%
       unlist(.)
-    
+
     all_plot_names <- plot_names %>%
       strsplit(., "\\+") %>%
       unlist(.) %>%
       str_to_title(.)
-    
-    
+
+
     data <- babynames %>%
       filter(is.element(name, all_plot_names)) %>%
       mutate(n = case_when(n >= input$minbirths ~ n)) %>%
       mutate(prop = case_when(n >= input$minbirths ~ prop)) %>%
       filter(input$xrange[1] <= year & year <= input$xrange[2])
-    
-    if (input$sex != "B") {data <- data %>%
-      filter(sex == input$sex)
+
+    if (input$sex != "B") {
+      data <- data %>%
+        filter(sex == input$sex)
     }
-    
+
     # for (i in length(plot_names)) {
     #   this_name_group <- strsplit(plot_names[i], "\\+") %>%
     #     unlist(.)
@@ -100,13 +107,13 @@ server <- function(input, output) {
     #     #data <- data %>% filter(!is.element(name, this_name_group))
     #   }
     # }
-    
+
     data <- data %>%
       mutate(name = paste0(name, " (", sex, ")"))
-    
+
     return(data)
   })
-  
+
   # Create plot
   output$main_plot <- renderPlot({
     if (input$yaxis == "prop") {
@@ -114,15 +121,17 @@ server <- function(input, output) {
     } else if (input$yaxis == "n") {
       yaxis_labels <- waiver()
     }
-    
+
     ggplot(data = filtered_data(), mapping = aes_string(x = "year", y = input$yaxis, group = "name")) +
       geom_line(aes(colour = name)) +
-      scale_x_continuous(limits = input$xrange,
-                         breaks = seq(input$xrange[1], input$xrange[2], 10)) +
+      scale_x_continuous(
+        limits = input$xrange,
+        breaks = seq(input$xrange[1], input$xrange[2], 10)
+      ) +
       scale_y_continuous(limits = c(0, NA), labels = yaxis_labels) +
       scale_color_brewer(palette = "Set1")
   })
-  
+
   # Create dataframe
   output$show_data <- DT::renderDataTable(filtered_data() %>% filter(!is.na(n)))
 }
